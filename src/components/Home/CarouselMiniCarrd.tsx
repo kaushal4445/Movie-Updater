5cf16cb8c9254ff6adc7328c8e16989513787ba7
Kaushal <mehrakaushal54@gmail.com>
2026-01-18 11:23:00 +0530

    Update CarouselMiniCarrd.tsx

--- src/components/Home/CarouselMiniCarrd.tsx
+++ src/components/Home/CarouselMiniCarrd.tsx
@@ -21,20 +21,20 @@ interface  CarouselMiniCarrdProps{
 
 
  
-function CarouselMiniCarrd({carouselMovies, item, ind}:  CarouselMiniCarrdProps) {
+function CarouselMiniCarrd({ item, ind}:  CarouselMiniCarrdProps) {
        const[hover, setHover] = useState<number | null>(null)
   return (
-     <Link to={`/details/${carouselMovies[item].id}`}>
+     <Link to={`/details/${[item].id}`}>
                     <div  key = {ind} className="flex  gap-2"
                     onMouseEnter={() => setHover(ind)}
                     onMouseLeave={() => setHover(null)}
                     >
                  
-                 <img src={imagepath + carouselMovies[item]?.poster_path}  className =" w-[100px]"alt="" />
+                 <img src={imagepath + [item]?.poster_path}  className =" w-[100px]"alt="" />
                  <div className="flex flex-col justify-between py-2">
                  <div className="leading-5">
-                    <h1 className={`${hover===ind ? "underline":"" }`}>{carouselMovies[item]?.title}</h1>
-                    <h1 className="text-md  text-zinc-300 line-clamp-3">{carouselMovies[item]?.overview}</h1>
+                    <h1 className={`${hover===ind ? "underline":"" }`}>{[item].title}</h1>
+                    <h1 className="text-md  text-zinc-300 line-clamp-3">{[item].overview}</h1>
                     </div>
                     <div className="flex gap-1 text-center">  <LuThumbsUp />
                         <h2>{carouselMovies[item]?.vote_count}</h2>
